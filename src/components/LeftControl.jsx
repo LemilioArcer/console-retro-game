720b8170bb181ab344e579d4057770a276c1ac5c
Arce <emilioarce1285@gmail.com>
2026-04-06 20:14:44 -0600

    First

---
+++ src/components/LeftControl.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LeftControl() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LeftControl
---
+++ src/components/RightControl.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RightControl() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RightControl
